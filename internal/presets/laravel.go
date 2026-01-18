9487f467a3ea1fe49ca3ba5c770f37e920ce55bc
Michael Dyrynda <michael@dyrynda.com.au>
2026-01-18 19:11:01 +1030

    sort laravel scaffolding step

--- internal/presets/laravel.go
+++ internal/presets/laravel.go
@@ -20,12 +20,12 @@ func NewLaravel() *Laravel {
 			defaultSteps: []config.StepConfig{
 				{Name: "php.composer", Args: []string{"install"}},
 				{Name: "node.npm", Args: []string{"ci"}},
+				{Name: "file.copy", From: ".env.example", To: ".env"},
 				{Name: "database.create"},
 				{Name: "php.laravel.artisan", Args: []string{"key:generate"}, Condition: map[string]interface{}{"env_not_exists": "APP_KEY"}},
-				{Name: "file.copy", From: ".env.example", To: ".env"},
 				{Name: "php.laravel.artisan", Args: []string{"migrate:fresh", "--seed"}, Condition: map[string]interface{}{"env_exists": "DB_CONNECTION"}},
-				{Name: "node.npm", Args: []string{"run", "build"}, Priority: 15},
 				{Name: "php.laravel.artisan", Args: []string{"storage:link"}},
+				{Name: "node.npm", Args: []string{"run", "build"}, Priority: 15},
 				{Name: "herd", Args: []string{"link", "--secure"}},
 			},
 			cleanupSteps: []config.CleanupStep{
